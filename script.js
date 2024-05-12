1c647a0639e1bdb35caf1494e5903dccf5a4b8f6
Dhruvin Patel <136917226+codewithdhruvinpatel@users.noreply.github.com>
2024-05-12 23:22:22 +0530

    script.js

--- script.js
+++ script.js
@@ -39,3 +39,27 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 
+     function sendEmail() {
+            Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "codewithdhruvinpatel@gmail.com",
+    Password : "D586076099D19D4D069A41BC31B6CA20EF4D",
+    To : 'codewithdhruvinpatel@gmail.com',
+    From : "codewithdhruvinpatel@gmail.com",
+    Subject : document.getElementById("exampleFormControlInput3").value,
+    Body : `
+    Name: ${document.getElementById("exampleFormControlInput1").value} <br>
+    Email: ${document.getElementById("exampleFormControlInput2").value} <br>
+    Message: ${document.getElementById("exampleFormControlTextarea4").value} <br>
+
+    `
+}).then(
+  message => alert("Email Not Sent W'll Be Back Soon , Mail Us At codewithdhruvinpatel@gmail.com")
+);
+
+
+        }
+     document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
+
